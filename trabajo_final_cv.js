9088eb137fec666904858040925c1c277a1df750
Argentina Programa 4.0 <127353407+luchodallas@users.noreply.github.com>
2023-03-08 19:20:03 -0300

    initial commit

---
+++ trabajo_final_cv.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById ('boton').onclick = function name() {
+
+    onclick=document.getElementById('info').innerHTML = "Tel 0295-1-4798<br>coqueluche@gmail.com<br>@labofantastico<br>Santa fe 3596 Chaco, Argentina"
+
+}
+ 
